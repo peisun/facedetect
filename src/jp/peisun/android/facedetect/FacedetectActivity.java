a57b775c47c8c536e79f712cb74d4cc42caa441d
peisun <peisunstar@gmail.com>
2012-01-19 15:58:19 +0900

    Camera関係の機能をCameraSurfaceに移動

--- src/jp/peisun/android/facedetect/FacedetectActivity.java
+++ src/jp/peisun/android/facedetect/FacedetectActivity.java
@@ -1,16 +1,7 @@
 package jp.peisun.android.facedetect;
 
-import java.util.List;
-
 import android.app.Activity;
-import android.content.res.Configuration;
-import android.hardware.Camera;
-import android.hardware.Camera.CameraInfo;
-import android.hardware.Camera.Size;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
@@ -19,185 +10,51 @@ import android.widget.FrameLayout;
 public class FacedetectActivity extends Activity {
 
 	/* カメラSurface関連 */
-	private SurfaceView mCameraView = null;
+	private CameraSurfaceView mCameraView = null;
+	
+	/* レイアウト */
 	private FrameLayout preview= null;
-	private SurfaceHolder mCameraSurfaceHolder = null;
 	
-	/* カメラ関連 */
-	private Camera mCamera = null;
-	private int mDefaultCameraId = 0;
-	private volatile boolean mPortrait = false;
-	
+
 	/* OverlayView */
 	OverlayView mOverlayView = null;
-	private final String TAG = "facedetect:activity";
-    @Override
-	public void onWindowFocusChanged(boolean hasFocus) {
-		// TODO �����������ꂽ���\�b�h�E�X�^�u
-		super.onWindowFocusChanged(hasFocus);
-		//mSize.width = findViewById(R.id.linearLayout1).getWidth();
-		//mSize.height = findViewById(R.id.linearLayout1).getHeight();
-		
-		
-		
-	}
-    
+
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        /* フルスクリーンモードにする */
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        /* タイトルを消す */
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        
-      /* カメラのSurfaceを作る */
-        //mCameraView = new CameraSurfaceView(this);
-        mCameraView = new SurfaceView(this);
-        mCameraSurfaceHolder = mCameraView.getHolder();
-        // SurfaceViewにリスナーを登録
-        mCameraSurfaceHolder.addCallback(surfaceListener);
-        mCameraSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        //mCameraSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
-        setContentView(mCameraView);
-        
-        mOverlayView = new OverlayView(this);
-        addContentView(mOverlayView, new LayoutParams(LayoutParams.FILL_PARENT,
-                LayoutParams.FILL_PARENT));
-        mOverlayView.setZOrderOnTop(true);
-        
-        
-    }
-    private int getCameraFacing(){
-    	int cameraId = 0;
-    	// カメラの個数を取得
-		int numberOfCameras = Camera.getNumberOfCameras();
-		// フロントカメラがあれば、それをデフォルトとする
-		CameraInfo cameraInfo = new CameraInfo();	
-		for (int i = 0; i < numberOfCameras; i++) {
-			Camera.getCameraInfo(i, cameraInfo);
-			if (cameraInfo.facing == CameraInfo.CAMERA_FACING_FRONT) {
-				cameraId = i;
-			}
-		}
-		return cameraId;
-    }
-    private Camera.PreviewCallback previewListener = new Camera.PreviewCallback(){
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-		@Override
-		public void onPreviewFrame(byte[] data, Camera camera) {
-			// TODO 自動生成されたメソッド・スタブ
-			Size s = camera.getParameters().getPreviewSize();
-			
-			mOverlayView.startFindFace(data,s.width,s.height,mPortrait);
-			//mOverlayView.startFindFace2(data,s.width,s.height,portrait);
-			long sleepTtime = mOverlayView.getFindFaceTime();
-//			try{
-//			Thread.sleep(sleepTtime);
-//			}
-//			catch(Exception e){
-//				
-//			}
-			mCamera.setOneShotPreviewCallback(previewListener);
-		}
-    	
-    };
-    private SurfaceHolder.Callback surfaceListener = new SurfaceHolder.Callback() {
-        // SurfaceViewが生成されたらカメラをオープンする
-    	@Override
-        public void surfaceCreated(SurfaceHolder holder) {
-        	/* フロントカメラを探す */
-    		mDefaultCameraId = getCameraFacing();
-        	mCamera = Camera.open(mDefaultCameraId);
-            try {
-            	mCamera.setPreviewDisplay(holder);
-            } catch (Exception e) {
-            	mCamera.release();
-                mCamera = null;
-                e.printStackTrace();
-            }
-        }
-        // SurfaceViewが破棄されるタイミングでカメラを開放する
-    	@Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-    		mCamera.setOneShotPreviewCallback(null);
-        	mCamera.stopPreview();
-        	mCamera.release();
-        	mCamera= null;
-        }
-    	
-    	@Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                int height) {
-    		
-    		Camera.Parameters parameters = mCamera.getParameters();
+		/* フルスクリーンモードにする */
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		/* タイトルを消す */
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-            List<Size> sizes = parameters.getSupportedPreviewSizes();
-            Size optimalSize = getOptimalPreviewSize(sizes, width, height);
-            parameters.setPreviewSize(optimalSize.width, optimalSize.height);
-            
-            mCamera.setParameters(parameters);
-            
-            mPortrait = (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT);
-    		if (mPortrait) {
-    			mCamera.setDisplayOrientation(90);  /* 縦向き */
-    		}
-    		else{
-    			mCamera.setDisplayOrientation(0); /* 横向き */
-    		}
-             
-            
-            //mOverlayView.setPreviewSize(optimalSize.width,optimalSize.height);
-            mOverlayView.createFaceDetector(width,height,mPortrait);
-            mCamera.setOneShotPreviewCallback(previewListener);
-        	mCamera.startPreview();
-        }
-    	private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
-            final double ASPECT_TOLERANCE = 0.05;
-            double targetRatio = (double) w / h;
-            if (sizes == null) return null;
+		setContentView(R.layout.main);
+		/* カメラのSurfaceを作る */
+		mCameraView = new CameraSurfaceView(this);
+		preview = (FrameLayout) findViewById(R.id.camera_preview);
+		preview.addView(mCameraView);
 
-            Size optimalSize = null;
-            double minDiff = Double.MAX_VALUE;
+		mOverlayView = new OverlayView(this);
+		preview.addView(mOverlayView, new LayoutParams(LayoutParams.FILL_PARENT,
+				LayoutParams.FILL_PARENT));
+		mOverlayView.setZOrderOnTop(true);
+		mCameraView.setOverlayView(mOverlayView);
 
-            int targetHeight = h;
+	}
 
-            // Try to find an size match aspect ratio and size
-            for (Size size : sizes) {
-                double ratio = (double) size.width / size.height;
-                if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
-                if (Math.abs(size.height - targetHeight) < minDiff) {
-                    optimalSize = size;
-                    minDiff = Math.abs(size.height - targetHeight);
-                }
-            }
-
-            // Cannot find the one match the aspect ratio, ignore the requirement
-            if (optimalSize == null) {
-                minDiff = Double.MAX_VALUE;
-                for (Size size : sizes) {
-                    if (Math.abs(size.height - targetHeight) < minDiff) {
-                        optimalSize = size;
-                        minDiff = Math.abs(size.height - targetHeight);
-                    }
-                }
-            }
-            return optimalSize;
-        }
-    };
 	@Override
 	protected void onPause() {
 		super.onPause();
-		
+
 	}
 
 	@Override
 	protected void onResume() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onResume();
-		
-		
+
+
 	}
-	
+
 }
